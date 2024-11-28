0663f121d5b4949c1ba2e39c81cc9622720fcd5e
Ablvsy <abegailflorescamacho0616@gmail.com>
2024-11-28 15:44:27 +0800

    Program 3.cpp

--- Program 3.cpp
+++ Program 3.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 int main() {
     int number1, number2;
-    int number1, number2, result;
     cout<<"Please enter your first number: ";
     cin>>number1; 
     cout<<"Please enter your second number: ";
     cin>>number2; 
-    cout<<"Product of the two numbers is "<<number1 * int number1, number2, result;
+    cout<<"Product of the two numbers is "<<number1 * number2;
 }
